18a17a8c6228edd733192ef47fae9c97052a0212
min:D_git <65944335+minD9it@users.noreply.github.com>
2025-04-12 17:42:31 +0900

    Title: 미지의 공간 탈출, Time: 77ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250412/미지의 공간 탈출/escape-unknown-space.py
+++ 250412/미지의 공간 탈출/escape-unknown-space.py
@@ -6,7 +6,7 @@ def time_start(arr):
     for i in range(length):
         for j in range(length):
             if arr[4][i][j] == 2: # 윗면의 좌표 중에 2가 있으면 그 곳이 시작 위치
-                return, 4, i, j
+                return 4, i, j
     
 
 # 미지의 공간에서의 탈출구
@@ -17,34 +17,91 @@ def unknown_end(arr):
             if arr[i][j] == 4:
                 return i, j
 
-# 시간의 벽 탈출 -> 미지의 공간 시작
-def time2unknow(arr, arr2, m):
-    ti, tj = 0, 0
+def timewall_loc(arr):
     length = len(arr)
     for i in range(length):
         for j in range(length):
             if arr[i][j] == 3:
-                ti, tj = i-1, j-1
-                break
-    # m X m 크기의 시간의 벽이기 때문에 그 주변에 1이 아니고 0인 곳을 찾아야 함, 시간의 벽 면 위치도 같이 파악해야 함
+                return i, j
+
+# 시간의 벽 탈출 -> 미지의 공간 시작
+def time2unknow(arr, m): # 미지의 공간, 시간의 벽, 시간의 벽 크기
+    # 미지의 공간에서 시간의 벽이 시작하는 위치
+    si, sj = timewall_loc(arr)
+    
+    # m X m 크기의 시간의 벽이기 때문에 그 주변에 1이 아니고 0인 곳을 찾아야 함, 시간의 벽: 면 위치도 같이 파악해야 함
     d = 0 # 동서남북 중 하나 (0,1,2,3)
     ui, uj = 0, 0
-    for i in range(m+2): # m + 2: 주변을 둘러싸고 있는 벽
+    for i in range(-1, m+1): # (-1,0,1,2,3) 주변을 둘러싸고 있는 벽
         if i == 0: d = 3 # 북
         elif i == m+1: d = 2 # 남
         for j in range(m+2):
             if j == 0:d = 1 # 서
             elif j == m+1: d = 0 # 동
-            if arr[ti+i][tj+j] == 0:
+            if arr[si+i][sj+j] == 0:
                 ui, uj = i, j
 
     # 시간의 벽 탈출구: 행은 항상 같음 , [d][m-1][?]
     ti, tj = 0, 0
-    if d == 0 or d == 2: # 동, 남
-        ti, tj = m-1, j
-    elif d == 1 or d == 3: # 서, 북
-        return d, m-1, i
+    if d == 0: # 동, 행 반대로
+        ti, tj = m-1, ui-si
+    elif d == 1: # 서, 행-1
+        ti, tj = m-1, ui-1
+    elif d == 2: # 남, 열-1
+        ti, tj = m-1, uj-1
+    elif d == 3: # 북, 열 반대로
+        ti, tj = m-1, uj-sj
+    else:
+        return -1
+
+    return d, ti, tj, ui, uj
+
+
+def time_bfs(arr, sd, si, sj, ed, ei, ej):
+    length = len(arr)
+    v = [[[0] * len(arr) for _ in range(len(arr))] for _ in range(5)] # 동서남북위 -> 각각 m X m 배열의 방문 그래프
+
+    q = deque([sd, si, sj])
+    v[sd][si][sj] = 1
+
+    while q: 
+        qd, qi, qj = deque.popleft()
+
+        # 상하좌우로 한 칸 이동: 다른 면으로 이동, 범위 벗어남, 막힌 길
+        for di, dj in ((-1,0), (1,0), (0,-1), (0,1)):
+            ni, nj = qi+di, qj+dj
+
+            if 0<=ni<length and 0<=nj<length and arr[ni][nj] == 0:
+                q.append([ni, nj])
+                v[ni][nj] = 1
+                
+
+    return -1
+        
+
+
+def unknown_bfs(dist, arr, si, sj, ei, ej):
+    length = len(arr)
+    v = [[0]*len(arr) for _ in range(len(arr))] # 미지의 공간 만큼의 방문 그래프
     
+    q = dqeue([si, sj])
+    v[si][sj] = 1
+        
+    while q: #
+        qi, qj = q.popleft()
+
+        # 상하좌우로 한 칸 이동: 범위 벗어남, 시간 이상 현상, 막힌 길(1)
+        for di, dj in ((-1,0), (1,0), (0,-1), (0,1)):
+            ni, nj = qi+di, qj+dj
+
+            # 범위 안, 이동 가능한 길
+            if 0<=ni<length and 0<=nj<length and arr[ni][nj] == 0:
+                q.append([ni, nj])
+                v[ni][nj] = 1
+                dist += 1
+    return -1
+
+            
 
 n, m, f = map(int, input().split()) # 미지의 공간, 시간의 벽, 시간 이상 현상 개수
 unknown = [list(map(int, input().split())) for _ in range(n)] # 미지의 공간 생성
@@ -53,13 +110,14 @@ anomaly = [list(map(int, input().split())) for _ in range(f)] # 시간 이상 �
 
 
 # 시작 위치, 종료 위치 찾기: 시간의 벽, 미지의 공간 각각의 위치
-t_si, t_sj = time_start(timewall)
-t_ei, t_ej = time_end(timewall)
-u_si, u_sj = unknown_start(unknown)
+t_sd, t_si, t_sj = time_start(timewall)
+t_ed, t_ei, t_ej, u_si, u_sj = time2unknow(unknown, m)
 u_ei, u_ej = unknown_end(unknown)
 
-def time_bfs(timewall, si, sj, ei, ej): # 시작 위치, 종료 위치
-    q = deque((si, sj))
+# 시간의 벽에서 팀색 진행
+tdist = time_bfs(timewall, t_sd, t_si, t_sj, t_ed, t_ei, t_ej)
 
+# 미지의 공간에서 탐색 진행
+rdist = unknown_bfs(tdist, unknown, u_si, u_sj, u_ei, u_ej)
 
-    
+print(rdist)
